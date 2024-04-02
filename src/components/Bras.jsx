4eac4bd23496ffdca79938b41333ceafd82562e5
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-04-02 13:10:11 +0200

    new commit

--- src/components/Bras.jsx
+++ src/components/Bras.jsx
@@ -1,8 +1,7 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
 
 const Bras = () => {
-  return <FontAwesomeIcon icon={faBars} />;
+  return <HiOutlineMenuAlt1 />;
 };
 
 export default Bras;
