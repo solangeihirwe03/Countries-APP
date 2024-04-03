b8842cbae90b61752871a74d5a1eee39ffa13b81
solangeihirwe03 <solangeduhimbazeihirwe@gmail.com>
2024-04-03 09:57:34 +0200

    country-api

--- src/components/Bras.jsx
+++
@@ -1,7 +0,0 @@
-import { HiOutlineMenuAlt1 } from "react-icons/hi";
-
-const Bras = () => {
-  return <HiOutlineMenuAlt1 />;
-};
-
-export default Bras;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,24 @@ import { SectionHolder } from '../styled-components/GeneralComponents'
 
 const Header = () => {
   return (
-    <section className="flex flex-col items-center text-white bg-[url('public/banner-background.jpg') bg-no-repeat]">
-      <SectionHolder className='mb-20 flex-col gap-10'>
+    <section className="flex flex-col items-center text-white bg-[url('https://cdn.pixabay.com/photo/2016/10/20/18/35/earth-1756274_1280.jpg')]">
+      <SectionHolder className="mb-20 flex-col gap-10">
         <NavBar />
-        <h1 className='text-4xl font-bold'>Country API App</h1>
-        <p className='text-xl'>This is a simple app that allows you to search for countries and continents using the API.</p>
-        <input type="text" name="search" id="search" placeholder='Search country by name' className='px-5 py-3 w-full md:w-1/2' />
+        <h1 className="text-4xl font-bold">Country API App</h1>
+        <p className="text-xl">
+          This is a simple app that allows you to search for countries and
+          continents using the API.
+        </p>
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Search country by name"
+          className="px-5 py-3 w-full md:w-1/2"
+        />
       </SectionHolder>
     </section>
-  )
+  );
 }
 
 export default Header
